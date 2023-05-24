efa4acb1a819dca4d6710d0b0664d49ab7182658
Jaime A. Mendez <jam65st@gmail.com>
2023-05-24 11:32:15 -0500

    Update after interview

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+/**
+ * Write a program that prints the numbers from 1 to 100.
+ * But for multiples of three print “Fizz” instead
+ * of the number and for the multiples of five print “Buzz”.
+ * For numbers which are multiples of both three and five print “FizzBuzz”
+ *
+ */
+
+const Numbers1to100 = () => {
+    let array = []
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 === 0 && i % 5 === 0) array.push('FizzBuzz');
+        else if (i % 3 === 0) array.push('Fizz');
+        else if (i % 5 === 0) array.push('Buzz');
+        else array.push(i);
+    }
+    return array;
+}
+
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({ name: Numbers1to100() });
 }
